3ab4c290bbffcc557b4ab0503b319a2e763a3368
Shane Winterhalter <shanetwinterhalter@gmail.com>
2023-04-24 20:25:10 +0100

    lots of ui work

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -2,6 +2,7 @@ import { uploadImage, generateImage, generateSegmentMask } from './loadImage.js'
 import { configureSaveButton } from './imageDownload.js';
 import { inpaintImage, upscaleImage } from './editImage.js';
 import { configureCanvas } from './createMask.js';
+import { setUserOptionsWidth, checkSidebarConfiguration } from './ui.js';
 
 // When upload button pressed
 $("#uploadImageForm").on("submit", function (event) {
@@ -9,19 +10,18 @@ $("#uploadImageForm").on("submit", function (event) {
     uploadImage();
 });
 
-$(document).ready(function() {
-    // When image generated
-    $("#generateImageButton").on("click", function () {
-        console.log("generate image");
-        generateImage();
-    });
-})
+// When image generated
+$("#generateImageButton").on("click", function () {
+    console.log("generate image");
+    generateImage();
+});
 
 // Listener to request segment mask whenever image updated
 const observer = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
         if (mutation.type === "attributes" && mutation.attributeName === "src") {
-            generateSegmentMask();
+            // Disable segment masks for now
+            //generateSegmentMask();
             configureCanvas();
         }
     });
@@ -44,4 +44,19 @@ $("#upscaleImageButton").on("click", function() {
 // On image saving
 $("#saveImageButton").on("click", function() {
     configureSaveButton();
+});
+
+// UI changes
+$(document).ready(function () {
+    setUserOptionsWidth();
+
+    // Select all buttons in the sidebar and attach a click event handler
+    $("#sidebar .btn").on("click", function () {
+        if ($(this).hasClass("selected-button")) {
+            $(this).removeClass("selected-button");
+        } else {
+            $(this).addClass("selected-button");
+        }
+        checkSidebarConfiguration();
+    });
 });
--- web/static/js/ui.js
+++ web/static/js/ui.js
@@ -1,3 +1,15 @@
+function isAnyButtonSelected() {
+    const buttons = $("#sidebar .btn");
+    let isSelected = false;
+    
+    buttons.each((_, button) => {
+      if ($(button).hasClass("selected-button")) {
+        isSelected = true;
+      }
+    });
+    return isSelected;
+}
+
 export function updateImage(imageUrl) {
     const img = $("#resultImage")
     img.attr("src", imageUrl);
@@ -12,5 +24,32 @@ export function disableUi(enable) {
     } else {
         $("#loader").hide();
         $(".submitButton").prop("disabled", false);
+    }
+}
+
+export function setUserOptionsWidth() {
+    const userOptions = $("#userOptions");
+    const children = userOptions.find(".user-input-section");
+    let maxWidth = 0;
+    userOptions.show();
+  
+    children.each((_, child) => {
+      const $child = $(child);
+      $child.css("display", "block"); // Temporarily display the element to measure its width
+      const childWidth = $child[0].getBoundingClientRect().width;
+      if (childWidth > maxWidth) {
+        maxWidth = childWidth;
+      }
+      $child.css("display", ""); // Reset display property
+    });
+    userOptions.css("width", maxWidth + "px");
+    userOptions.hide();
+}
+
+export function checkSidebarConfiguration() {
+    if (isAnyButtonSelected()) {
+        $("#userOptions").show();
+    } else {
+        $("#userOptions").hide();
     }
 }
